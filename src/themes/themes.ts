f3a69a09516392ad1f0a625c7a3acbccd545309d
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-10-31 16:20:35 -0300

    chore: initial config

---
+++ src/themes/themes.ts
@@ -0,0 +1,23 @@
+export const Colors = {
+  primary: "#FF577F",
+  primaryHover: "#FF427F",
+  secundary: "#59323F",
+  gray1: "#121214",
+  gray2: "#212529",
+  gray3: "#343B41",
+  gray4: "#868E96",
+  gray5: "#F8F9FA",
+
+  feedback: {
+    success: "#3FE864",
+    negative: "#E83F5B",
+  },
+
+  text: {
+    primary: "#FF577F",
+    gray0: "#F8F9FA",
+    white: "#FFFFFF",
+    gray1: "#868E96",
+    gray50: "#868E96",
+  },
+};
