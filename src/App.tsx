ab8fee11ac03b9e31eef3e906715c807bdc00f28
VictorHugo110199 <victorhugo110199@gmail.com>
2022-11-01 19:12:56 -0300

    fix-App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './styles/main.css';
-import  { Input }  from './components/Input';
+
 
 export const App = () => {
   return (
     <div>
-      <h1>teste</h1>
-      <Input placeholder='Digite seu nome aqui...' name='nome' type='text'></Input>
+
     </div>
   );
 };
