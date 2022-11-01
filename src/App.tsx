ee8a02fcc2322f3679eb8c9fe93e179e614d466d
VictorHugo110199 <victorhugo110199@gmail.com>
2022-11-01 18:46:47 -0300

    feat-modal-input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './styles/main.css';
+import  { Input }  from './components/Input';
 
 export const App = () => {
   return (
     <div>
       <h1>teste</h1>
+      <Input placeholder='Digite seu nome aqui...'></Input>
     </div>
   );
 };
