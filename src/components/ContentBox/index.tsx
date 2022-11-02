021685f8a9f115edc85db972260e78966e61df7b
matheusfelipetp <matheusfelipetp@outlook.com>
2022-11-02 17:54:04 -0300

    fix: ContentBox, ContentBoxDiv and HomePage

--- src/components/ContentBox/index.tsx
+++ src/components/ContentBox/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode } from 'react';
 
 interface iContentBox {
   children: ReactNode;
@@ -8,10 +8,10 @@ interface iContentBox {
 
 export const ContentBox = ({ children, title, text }: iContentBox) => {
   return (
-    <div className=" min-w-[360px] w-[380px] h-[224px] p-8 border-2 border-green-100 rounded-[10px] bg-gray-300 flex flex-col gap-5">
+    <div className=" min-w-[360px] w-[380px] h-[224px] p-8 border-2 border-green-100 rounded-[10px] bg-gray-500 flex flex-col justify-center gap-5">
       <header className="flex items-center gap-5">
         {children}
-        <h2 className="text-green-100 text-3xl font-medium"> {title} </h2>
+        <h2 className="text-green-100 text-3xl font-medium"> {title}</h2>
       </header>
       <p className="text-white pl-3">{text}</p>
     </div>
--- src/components/ContentBoxDiv/index.tsx
+++ src/components/ContentBoxDiv/index.tsx
@@ -1,50 +1,51 @@
-import { ContentBox } from "../ContentBox";
-import { AiOutlineTrophy } from "react-icons/ai";
-import { IoCreateOutline, IoStatsChartOutline } from "react-icons/io5";
-import { BsPersonLinesFill } from "react-icons/bs";
-import { VscDebugRestart } from "react-icons/vsc";
-import { TbTournament } from "react-icons/tb";
+import { ContentBox } from '../ContentBox';
+import { AiOutlineTrophy } from 'react-icons/ai';
+import { IoCreateOutline, IoStatsChartOutline } from 'react-icons/io5';
+import { BsPersonLinesFill } from 'react-icons/bs';
+import { VscDebugRestart } from 'react-icons/vsc';
+import { TbTournament } from 'react-icons/tb';
 
 export const ContentBoxDiv = () => {
-  // Quando chamar o children utilizar essa class => className="text-green-100 text-5xl"
+  const classBox = 'text-green-100 text-5xl';
+
   return (
-    <div className="flex justify-center bg-transparent">
-      <div className="flex overflow-x-scroll laptop:flex-wrap laptop:justify-center laptop:bg-transparent laptop:max-w-6xl mb-[50px]">
+    <div className="flex justify-center">
+      <div className="max-w-7xl m-auto px-4 py-5 flex gap-10 laptop:justify-evenly overflow-x-scroll laptop:overflow-hidden laptop:flex-wrap  laptop:bg-transparent laptop:w-full mb-[120px]">
         <ContentBox
           title="Crie torneios"
           text="Convide seus amigos e gerencie campeonatos."
         >
-          <IoCreateOutline className="text-green-100 text-5xl" />
+          <IoCreateOutline className={classBox} />
         </ContentBox>
         <ContentBox
           title="Estatísticas"
           text="Tenha acesso a dados e estatísticas dos participantes do seu torneio."
         >
-          <IoStatsChartOutline className="text-green-100 text-5xl" />
+          <IoStatsChartOutline className={classBox} />
         </ContentBox>
         <ContentBox
           title="Fácil acesso"
           text="Basta que uma pessoa crie a conta para ter acesso e gerenciar o torneio."
         >
-          <BsPersonLinesFill className="text-green-100 text-5xl" />
+          <BsPersonLinesFill className={classBox} />
         </ContentBox>
         <ContentBox
           title="Histórico"
           text="Tenha acesso ao histórico de campeões dos seus torneios."
         >
-          <AiOutlineTrophy className="text-green-100 text-5xl" />
+          <AiOutlineTrophy className={classBox} />
         </ContentBox>
         <ContentBox
           title="Reinicie"
           text="Reinicie um torneio, após finalizá-lo, sem precisar preencher os dados novamente."
         >
-          <VscDebugRestart className="text-green-100 text-5xl" />
+          <VscDebugRestart className={classBox} />
         </ContentBox>
         <ContentBox
           title="Versátil"
           text="Para todos os tipos de ocasiões e campeonatos. A única regra é se divertir."
         >
-          <TbTournament className="text-green-100 text-5xl" />
+          <TbTournament className={classBox} />
         </ContentBox>
       </div>
     </div>
