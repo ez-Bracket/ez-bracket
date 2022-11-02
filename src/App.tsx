8f92394193cfbb59ffe1077b7673d238e9e78298
VictorHugo110199 <victorhugo110199@gmail.com>
2022-11-02 14:42:09 -0300

    feat:Api Login Register

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useContext, useEffect } from 'react';
+import { UserContext } from './contexts/UserContext';
 import { RoutesApp } from './routes';
 import './styles/main.css';
 
 export const App = () => {
+
+  const {Login, Register, user} = useContext(UserContext)
+
+  useEffect(() => {
+    Login({
+      email: "victorteste@gmail.com",
+      password: "teste123",
+    })
+    console.log(user)
+  }, []) 
+
   return (
     <div>
       <RoutesApp />
