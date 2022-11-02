363ce27568b1b3202f1e176ab030e2bc94dba133
hideo651 <hideo651@msn.com>
2022-11-01 22:47:26 -0300

    feat: add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import "./styles/main.css";
-import { FooterHomePage } from "./components/footer";
 
 export const App = () => {
-  return (
-    <div className="bg-gray-300">
-      <FooterHomePage />
-    </div>
-  );
+  return <div></div>;
 };
