5ccb6a23506468e1a494990ae8c641019bf74542
VictorHugo110199 <victorhugo110199@gmail.com>
2022-11-02 14:44:48 -0300

    fix: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { useContext, useEffect } from 'react';
-import { UserContext } from './contexts/UserContext';
 import { RoutesApp } from './routes';
 import './styles/main.css';
 
 export const App = () => {
-
-  const {Login, Register, user} = useContext(UserContext)
-
-  useEffect(() => {
-    Login({
-      email: "victorteste@gmail.com",
-      password: "teste123",
-    })
-    console.log(user)
-  }, []) 
 
   return (
     <div>
