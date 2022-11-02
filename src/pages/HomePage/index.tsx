0ca074fc4a7327ed078cd2aad3a89bb4b0cb25e3
Enrico Vieira <enricovieira@hotmail.com>
2022-11-02 16:18:30 -0300

    feat: add ContentBox component and ContentBoxDiv component

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,11 +1,15 @@
-import { Footer } from '../../components/Footer';
-import { Header } from '../../components/Header';
-import { HomeBrackets } from '../../components/HomeBrackets';
+import { ContentBox } from "../../components/ContentBox";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { HomeBrackets } from "../../components/HomeBrackets";
+import { AiOutlineTrophy } from "react-icons/ai";
+import { ContentBoxDiv } from "../../components/ContentBoxDiv";
 
 export const HomePage = () => {
   return (
     <>
       <Header />
+      <ContentBoxDiv />
       <HomeBrackets />
       <Footer />
     </>
