5a64f2a9b0179a07712afc9fc1c0d66df3ebb40f
matheusfelipetp <matheusfelipetp@outlook.com>
2022-11-02 17:17:03 -0300

    fix: Header, HomePage, ModalLogin and ModalRegister; feat: add MessageError

--- src/components/ModalLogin/index.tsx
+++ src/components/ModalLogin/index.tsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from "react";
-import { BsEye, BsEyeSlash } from "react-icons/bs";
+import { useRef, useState } from 'react';
+import { BsEye, BsEyeSlash } from 'react-icons/bs';
 import {
   Modal,
   ModalOverlay,
@@ -8,32 +8,27 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  useDisclosure,
   Button,
   FormControl,
   FormLabel,
   Input,
   InputGroup,
   InputRightElement,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
-export const ModalLogin = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+interface IModalLogin {
+  isOpen: boolean;
+  onClose: () => void;
+}
 
+export const ModalLogin = ({ isOpen, onClose }: IModalLogin) => {
   const [showPass, setShowPass] = useState(false);
-
-  const handleShowPass = () => setShowPass(!showPass);
-
   const initialRef = useRef(null);
   const finalRef = useRef(null);
+  const handleShowPass = () => setShowPass(!showPass);
 
   return (
     <>
-      <Button onClick={onOpen}>Open Modal</Button>
-      <Button ml={4} ref={finalRef}>
-        I'll receive focus on close
-      </Button>
-
       <Modal
         initialFocusRef={initialRef}
         finalFocusRef={finalRef}
@@ -42,62 +37,66 @@ export const ModalLogin = () => {
       >
         <ModalOverlay />
         <ModalContent
-          maxW={573}
-          m="auto"
+          maxW={550}
+          mx={4}
+          p={3}
+          px={[0, 3]}
           border="2px solid #61FFAA"
           bg="#221E34"
         >
-          <div className="m-auto ">
-            <ModalHeader className="text-green-100   mt-10 mb-0">
-              <p className="text-3xl">Entre na sua conta</p>
+          <div className="m-auto text-xl">
+            <ModalHeader className="text-green-100">
+              <h2 className="text-xl tablet:text-2xl">Entre na sua conta</h2>
             </ModalHeader>
 
             <ModalCloseButton
-              className="mt-6 bg-green-100 text-gray-300"
+              className="mt-3 mr-2 bg-green-100 text-gray-300"
               borderRadius={50}
-              _hover={{ bg: "#38F892" }}
-              transition="0.5s ease"
+              h={6}
+              w={6}
+              _hover={{ bg: '#38F892' }}
+              transition="0.3s ease"
             />
           </div>
 
           <form className="w-[100%]">
-            <ModalBody className="mt-8 mb-6 mx-auto">
-              <FormControl>
-                <FormLabel className="text-green-100">
+            <ModalBody className=" mt-1 laptop:mt-4 mb-4">
+              <FormControl position="relative">
+                <FormLabel fontSize={16} className="text-green-100">
                   Nome de usuário
                 </FormLabel>
                 <Input
                   placeholder="Digite o nome de usuário"
-                  _placeholder={{ color: "#c7c7c7" }}
+                  _placeholder={{ color: '#c7c7c7', opacity: '50%' }}
                   borderColor="#353149"
                   bg="#353149"
-                  height="60px"
+                  fontSize="14px"
+                  height="50px"
                   color="#c7c7c7"
                 />
               </FormControl>
 
               <FormControl mt={4}>
-                <FormLabel className="text-green-100">
-                  Senha
-                </FormLabel>
+                <FormLabel className="text-green-100">Senha</FormLabel>
                 <InputGroup>
                   <Input
                     placeholder="Digite sua senha"
-                    _placeholder={{ color: "#c7c7c7" }}
+                    _placeholder={{ color: '#c7c7c7', opacity: '50%' }}
                     borderColor="#353149"
                     bg="#353149"
-                    height="60px"
+                    fontSize="14px"
+                    height="50px"
                     color="#c7c7c7"
-                    type={showPass ? "text" : "password"}
+                    type={showPass ? 'text' : 'password'}
                   />
                   <InputRightElement>
                     <Button
                       onClick={handleShowPass}
                       bg="transparent"
                       padding={0}
-                      _hover={{ bg: "transparent" }}
-                      _active={{ bg: "transparent" }}
-                      top="10px"
+                      _hover={{ bg: 'transparent' }}
+                      _active={{ bg: 'transparent' }}
+                      top="5px"
                       right="10px"
                     >
                       {showPass ? (
@@ -116,25 +115,24 @@ export const ModalLogin = () => {
               flexDirection="column"
               justifyContent="space-evenly"
               w="100%"
-              mb={10}
               color="#c7c7c7"
               fontSize="14px"
-              paddingBottom=""
             >
               <Button
+                type="submit"
                 bg="#61FFAA"
                 color="#221E34"
                 fontWeight="500"
+                fontSize="18px"
                 w="100%"
                 h="49px"
-                mb={10}
+                mb={5}
                 _hover={{
-                  bg: "#38F892",
-                  fontWeight: "700",
+                  bg: '#38F892',
                 }}
-                transition="0.9s ease"
+                transition="0.3s ease"
               >
-                Save
+                Entrar
               </Button>
               <p>Já possui conta? Faça o login</p>
             </ModalFooter>
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,13 +1,30 @@
+import { useDisclosure } from '@chakra-ui/react';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { HomeBrackets } from '../../components/HomeBrackets';
+import { ModalLogin } from '../../components/ModalLogin';
+import { ModalRegister } from '../../components/ModalRegister';
 
 export const HomePage = () => {
+  const {
+    isOpen: isOpenRegister,
+    onOpen: onOpenRegister,
+    onClose: onCloseRegister,
+  } = useDisclosure();
+  
+  const {
+    isOpen: isOpenLogin,
+    onOpen: onOpenLogin,
+    onClose: onCloseLogin,
+  } = useDisclosure();
+
   return (
     <>
-      <Header />
-      <HomeBrackets />
+      <Header onOpenRegister={onOpenRegister} onOpenLogin={onOpenLogin} />
+      <HomeBrackets onOpen={onOpenRegister} />
       <Footer />
+      <ModalLogin isOpen={isOpenLogin} onClose={onCloseLogin} />
+      <ModalRegister isOpen={isOpenRegister} onClose={onCloseRegister} />
     </>
   );
 };
