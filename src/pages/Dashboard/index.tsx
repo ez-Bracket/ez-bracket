15a32b5648b4df9769479593d78d39606c7af26c
Enrico Vieira <enricovieira@hotmail.com>
2022-11-03 17:27:30 -0300

    feat: add DashboardTournament and DashboardMain components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,23 +1,23 @@
-import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
-import { DashboardMenu } from '../../components/DashboardMenu';
-import { UserContext } from '../../contexts/UserContext';
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { DashboardMenu } from "../../components/DashboardMenu";
+import { UserContext } from "../../contexts/UserContext";
+import { DashboardTournament } from "../../components/DashboardTournament";
 
 export const Dashboard = () => {
   const { user } = useContext(UserContext);
 
   return (
     <>
-
-      {user.length > 0  ? (
+      {user.length > 0 ? (
         <div className="bg-gray-300 h-screen w-screen">
           <div className="bg-dashboard bg-cover h-screen w-full absolute opacity-10"></div>
+          <DashboardTournament />
           <DashboardMenu />
         </div>
       ) : (
         <Navigate to="/" replace />
       )}
-    
     </>
   );
 };
