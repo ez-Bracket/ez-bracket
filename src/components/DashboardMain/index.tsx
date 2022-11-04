966f2bfde4d20fe8c06fe78f8dc5cfd38e62e5cf
Enrico Vieira <enricovieira@hotmail.com>
2022-11-04 18:26:55 -0300

    fix: dashboard box state

--- src/components/DashboardMain/index.tsx
+++ src/components/DashboardMain/index.tsx
@@ -1,27 +1,22 @@
-import { useContext, useEffect } from 'react';
-import { DashboardBox } from '../DashboardBox';
-import { AddBox } from '../AddBox';
-import { CampConext } from '../../contexts/CampContext';
-import { DashboardMainMessage } from '../DashboardMainMessage';
+import { useContext, useEffect } from "react";
+import { DashboardBox } from "../DashboardBox";
+import { AddBox } from "../AddBox";
+import { CampConext } from "../../contexts/CampContext";
+import { DashboardMainMessage } from "../DashboardMainMessage";
 
 export const DashboardMain = () => {
   const { camp, getCompetition } = useContext(CampConext);
-  const idUser = window.localStorage.getItem('@EZ:USERID');
+  const idUser = window.localStorage.getItem("@EZ:USERID");
 
   useEffect(() => {
     getCompetition(Number(idUser));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
+  }, [camp]);
 
   return camp.length ? (
     <div className="flex justify-center tablet:justify-start flex-wrap mx-4 tablet:mr-8 tablet:ml-44 gap-10">
       {camp.map((tournament) => (
-        <DashboardBox
-          tournament={tournament}
-          key={tournament.id}
-        />
-
+        <DashboardBox tournament={tournament} key={tournament.id} />
       ))}
       <AddBox />
     </div>
