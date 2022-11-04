f7f7871a65d65f2836977f6bd7740ae764798895
Pedro Silva <pedrosilvacwb@gmail.com>
2022-11-04 12:05:08 -0300

    feat: add feat add players page layout

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,11 +1,11 @@
-import { createContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { createContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // Utilities
-import { Api } from "../services/Api";
+import { Api } from '../services/Api';
 
 // Components
-import { CustomToast } from "../components/Toast";
+import { CustomToast } from '../components/Toast';
 
 interface iUserContextProps {
   children: React.ReactNode;
@@ -62,13 +62,9 @@ interface IuserApiGet {
   competition: [];
 }
 
-export const UserContext = createContext<IuserContext>(
-  {} as IuserContext
-);
+export const UserContext = createContext<IuserContext>({} as IuserContext);
 
-export const UserProvider = ({
-  children,
-}: iUserContextProps) => {
+export const UserProvider = ({ children }: iUserContextProps) => {
   const [user, setUser] = useState<IuserApiGet[]>([]);
 
   const [isLoading, setIsLoading] = useState(false);
@@ -78,8 +74,8 @@ export const UserProvider = ({
   const { toastify } = CustomToast();
 
   const LoadUser = async () => {
-    const token = localStorage.getItem("@EZ:TOKEN");
-    const id = localStorage.getItem("@EZ:USERID");
+    const token = localStorage.getItem('@EZ:TOKEN');
+    const id = localStorage.getItem('@EZ:USERID');
 
     if (token) {
       setIsLoading(true);
@@ -87,11 +83,11 @@ export const UserProvider = ({
         Api.defaults.headers.authorization = `Bearer ${token}`;
 
         const res = await Api.get<IuserApiGet>(
-          `users/${id}?_embed=competition`
+          `users/${id}?_embed=competition`,
         );
 
         setUser([res.data]);
-        navigate("/dashboard");
+        // navigate('/dashboard');
       } catch (error) {
         return error;
       } finally {
@@ -108,29 +104,19 @@ export const UserProvider = ({
   const Login = async (data: IuserDataLogin) => {
     try {
       setIsLoading(true);
-      const resp = await Api.post<IuserApiLoginResp>(
-        "login",
-        data
-      );
+      const resp = await Api.post<IuserApiLoginResp>('login', data);
       window.localStorage.clear();
-      window.localStorage.setItem(
-        "@EZ:TOKEN",
-        resp.data.accessToken
-      );
-      window.localStorage.setItem(
-        "@EZ:USERID",
-        resp.data.user.id
-      );
+      window.localStorage.setItem('@EZ:TOKEN', resp.data.accessToken);
+      window.localStorage.setItem('@EZ:USERID', resp.data.user.id);
       LoadUser();
       toastify({
-        description: "Login realizado com sucesso!",
-        status: "success",
+        description: 'Login realizado com sucesso!',
+        status: 'success',
       });
     } catch (error) {
       toastify({
-        description:
-          "E-mail ou senha inválido, tente novamente!",
-        status: "error",
+        description: 'E-mail ou senha inválido, tente novamente!',
+        status: 'error',
       });
       return error;
     } finally {
@@ -140,20 +126,16 @@ export const UserProvider = ({
 
   const Register = async (data: IuserDataRegister) => {
     try {
-      await Api.post<IuserApiRegisterResp>(
-        "register",
-        data
-      );
+      await Api.post<IuserApiRegisterResp>('register', data);
 
       toastify({
-        description: "Usuário cadastrado com sucesso!",
-        status: "success",
+        description: 'Usuário cadastrado com sucesso!',
+        status: 'success',
       });
     } catch (error) {
       toastify({
-        description:
-          "Ops, algo deu errado tente novamente!",
-        status: "error",
+        description: 'Ops, algo deu errado tente novamente!',
+        status: 'error',
       });
       return error;
     }
@@ -161,15 +143,13 @@ export const UserProvider = ({
 
   const Logout = () => {
     setUser([]);
-    window.localStorage.removeItem("@EZ:TOKEN");
-    window.localStorage.removeItem("@EZ:USERID");
-    navigate("/");
+    window.localStorage.removeItem('@EZ:TOKEN');
+    window.localStorage.removeItem('@EZ:USERID');
+    navigate('/');
   };
 
   return (
-    <UserContext.Provider
-      value={{ Login, Register, Logout, user, isLoading }}
-    >
+    <UserContext.Provider value={{ Login, Register, Logout, user, isLoading }}>
       {children}
     </UserContext.Provider>
   );
