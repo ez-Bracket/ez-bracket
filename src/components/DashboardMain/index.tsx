df8e394da5b89c779267b9032c17b20b767f7b07
Enrico Vieira <enricovieira@hotmail.com>
2022-11-04 13:49:42 -0300

    feat: add DashboardBox and Dashboard main functions, and create DashboardMainMessage

--- src/components/DashboardMain/index.tsx
+++ src/components/DashboardMain/index.tsx
@@ -1,11 +1,28 @@
-import { useContext } from "react";
-
-// Utilities
-import { UserContext } from "../../contexts/UserContext";
+import { useContext, useEffect } from "react";
+import { DashboardBox } from "../DashboardBox";
+import { AddBox } from "../AddBox";
+import { CampConext } from "../../contexts/CampContext";
+import { DashboardMainMessage } from "../DashboardMainMessage";
 
 export const DashboardMain = () => {
-  const { user } = useContext(UserContext);
-  console.log(user);
+  const { camp, getCompetition } = useContext(CampConext);
 
-  return <div></div>;
+  const idUser = window.localStorage.getItem("@EZ:USERID");
+
+  useEffect(() => {
+    getCompetition(Number(idUser));
+  }, []);
+
+  useEffect(() => {
+    console.log(camp);
+  }, [camp]);
+
+  return camp.length ? (
+    <div className="flex justify-center tablet:justify-start flex-wrap mx-4 tablet:mr-8 tablet:ml-44 gap-10">
+      {camp.map((tournament)=> <DashboardBox tournament={tournament}/> )}
+      <AddBox />
+    </div>
+  ) : (
+    <DashboardMainMessage />
+  );
 };
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,11 @@
 import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { DashboardMenu } from '../../components/DashboardMenu';
-import { AddBox } from '../../components/AddBox';
-import { DashboardBox } from '../../components/DashboardBox';
 import { InfoUserModal } from '../../components/ModalInfoUser';
 import { NewCampModal } from '../../components/ModalNewCamp';
 import { ModalRegister } from '../../components/ModalRegister';
 import { UserContext } from '../../contexts/UserContext';
+import { DashboardMain } from '../../components/DashboardMain';
 
 export const Dashboard = () => {
   const { user } = useContext(UserContext);
@@ -18,10 +17,7 @@ export const Dashboard = () => {
           <div className="bg-dashboard bg-cover h-screen w-full absolute opacity-10 shadow-[0_100px_100px_#070516]"></div>
           <section className="py-10 tablet:py-20 bg-gray-300">
             <DashboardMenu />
-            <div className="flex justify-center tablet:justify-start flex-wrap mx-4 tablet:mr-8 tablet:ml-44 gap-10">
-              <DashboardBox />
-              <AddBox />
-            </div>
+            <DashboardMain />
           </section>
         </div>
       ) : (
