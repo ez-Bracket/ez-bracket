1b219e9f3dfa5aec777f6415575760d25942190b
matheusfelipetp <matheusfelipetp@outlook.com>
2022-11-04 13:42:01 -0300

    fix: ModalRegister in Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,16 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
-import { DashboardMenu } from "../../components/DashboardMenu";
-import { AddBox } from "../../components/AddBox";
-import { DashboardBox } from "../../components/DashboardBox";
-import { InfoUserModal } from "../../components/ModalInfoUser";
-import { NewCampModal } from "../../components/ModalNewCamp";
+import { useContext } from 'react';
+import { Navigate } from 'react-router-dom';
+import { DashboardMenu } from '../../components/DashboardMenu';
+import { AddBox } from '../../components/AddBox';
+import { DashboardBox } from '../../components/DashboardBox';
+import { InfoUserModal } from '../../components/ModalInfoUser';
+import { NewCampModal } from '../../components/ModalNewCamp';
 
-import { ModalRegister } from "../../components/ModalRegister";
-import { UserContext } from "../../contexts/UserContext";
-import { ModalDeleteCamp } from "../../components/ModalDeleteCamp";
+import { ModalRegister } from '../../components/ModalRegister';
+import { UserContext } from '../../contexts/UserContext';
+import { ModalDeleteCamp } from '../../components/ModalDeleteCamp';
 
-import { ModalEdit } from "../../components/ModalEditUser";
-
+import { ModalEdit } from '../../components/ModalEditUser';
 
 export const Dashboard = () => {
   const { user } = useContext(UserContext);
@@ -33,7 +32,7 @@ export const Dashboard = () => {
         <Navigate to="/" replace />
       )}
       <InfoUserModal />
-      <ModalRegister title="Edite sua conta" buttonDesc="Editar" />
+      <ModalRegister />
       <ModalEdit />
       <NewCampModal />
       <ModalDeleteCamp />
