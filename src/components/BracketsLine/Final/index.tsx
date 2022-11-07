e070557121697e3e3339424a61b3cd2cbc836897
Enrico Vieira <enricovieira@hotmail.com>
2022-11-07 15:10:16 -0300

    fix: brackets responsivity

--- src/components/BracketsLine/Final/index.tsx
+++ src/components/BracketsLine/Final/index.tsx
@@ -6,13 +6,13 @@ export const LineFinal = ({ className }: IlineFinal) => {
   return (
     <div className={className}>
       <div className="flex justify-start items-start w-[58px] flex-wrap rounded">
-        <p className="w-[55px] h-[1.1px] bg-gray-100 mb-[200px] mobile:mb-[165px] mobile:mt-[40px] mobile:w-[65px]"></p>
-        <p className="w-[55px] h-[1.1px] bg-gray-100 mt-[180px] mobile:mt-[130px] mobile:mb-[40px] mobile:w-[65px]"></p>
+        <p className="w-[55px] h-[1.1px] bg-gray-100 mb-[200px] "></p>
+        <p className="w-[55px] h-[1.1px] bg-gray-100 mt-[180px] "></p>
       </div>
       <div className="-mx-[55px] rounded">
-        <p className="w-[225px] h-[1.1px] bg-gray-100 rotate-90 mb-[77px] -mx-[60px] mobile:w-[145px] mobile:-mx-[15px]"></p>
+        <p className="w-[225px] h-[1.1px] bg-gray-100 rotate-90 mb-[77px] -mx-[60px] "></p>
         <p className="w-[48px] h-[1.1px] bg-gray-100 mx-[55px]"></p>
-        <p className="w-[225px] h-[1.1px] bg-gray-100 rotate-90 mt-[77px] -mx-[60px] mobile:w-[145px] mobile:-mx-[15px]"></p>
+        <p className="w-[225px] h-[1.1px] bg-gray-100 rotate-90 mt-[77px] -mx-[60px] "></p>
       </div>
     </div>
   );
--- src/components/PlayersCard/Final/index.tsx
+++ src/components/PlayersCard/Final/index.tsx
@@ -12,35 +12,23 @@ export const Final = ({ className }: iFinalProps) => {
       {/* Se status for win, chamar este card */}
 
       <div className="w-52 h-[70px] flex border-2 bg-gray-400 border-green-100 rounded-md items-center justify-between px-4">
-        <img
-          src={imgteste}
-          alt=""
-          className="w-10 h-10 rounded-full"
-        />
+        <img src={imgteste} alt="" className="w-10 h-10 rounded-full" />
         <h2 className="text-sm text-green-100 leading-3 font-normal">
           FigmaTeam
         </h2>
-        <p className="text-sm text-green-100 leading-3 font-normal">
-          W
-        </p>
+        <p className="text-sm text-green-100 leading-3 font-normal">W</p>
       </div>
 
       {/* Se status for lose, chamar este card */}
 
       <div className="w-52 h-[70px] flex border-2 bg-gray-400 border-error-100 rounded-md items-center justify-between px-4">
-        <img
-          src={imgTeste}
-          alt=""
-          className="w-10 h-10 rounded-full"
-        />
+        <img src={imgTeste} alt="" className="w-10 h-10 rounded-full" />
         <h2 className="text-sm text-error-100 leading-3 font-normal">
           API Team
         </h2>
-        <p className="text-sm text-error-100 leading-3 font-normal">
-          L
-        </p>
+        <p className="text-sm text-error-100 leading-3 font-normal">L</p>
       </div>
-      <LineFinal className="flex justify-start items-center ml-52 -mt-[720px] mobile:-mt-[600px]" />
+      <LineFinal className="flex justify-start items-center ml-52 -mt-[681px] " />
     </div>
   );
 };
