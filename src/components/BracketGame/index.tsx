6825baf1e669c89956128d44234f264c0b43800c
matheusfelipetp <matheusfelipetp@outlook.com>
2022-11-07 15:37:48 -0300

    fix: Tournament Page

--- src/components/BracketGame/index.tsx
+++ src/components/BracketGame/index.tsx
@@ -1,19 +1,15 @@
-import { Final } from "../PlayersCard/Final";
-import { Semifinal } from "../PlayersCard/Semifinal";
-import { PlayersCard } from "../PlayersCard/WinAndLoseCards";
-import { WinnerCard } from "../PlayersCard/WinnerCard";
+import { Final } from '../PlayersCard/Final';
+import { Semifinal } from '../PlayersCard/Semifinal';
+import { PlayersCard } from '../PlayersCard/WinAndLoseCards';
+import { WinnerCard } from '../PlayersCard/WinnerCard';
 
-interface iTesteProps {
-  className?: string;
-}
-
-export const BracketGame = ({ className }: iTesteProps) => {
+export const BracketGame = () => {
   return (
-    <div className={className}>
-      <PlayersCard className="flex flex-col gap-[70px] " />
-      <Semifinal className="flex flex-col gap-[104px] mt-10 " />
-      <Final className="flex flex-col gap-[281px] mt-[135px] " />
-      <WinnerCard className="flex flex-col mt-36" />
-    </div>
+    <section className="flex pb-14 mt-8 max-w-[100%] overflow-auto mx-4 tablet:ml-48 relative">
+      <PlayersCard />
+      <Semifinal />
+      <Final />
+      <WinnerCard />
+    </section>
   );
 };
